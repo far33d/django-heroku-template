96b2b730a95e2780b89c65f0a06811928211f574
Fareed Mosavat <far33d@gmail.com>
2015-03-01 15:38:13 -0800

    Added csrf token passing for ajax by default.

---
+++ django_heroku_template/static/js/ajax.js
@@ -0,0 +1,18 @@
+// set up csrf protection for ajax requests
+//
+
+var csrftoken = $.cookie('csrftoken');
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
